1bb3261f34bca4333b11411d7aaef1d46f265b31
Goga270 <90174259+Goga270@users.noreply.github.com>
2022-08-10 12:37:12 +0300

    Выгрузка 2 дз

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -1,15 +1,24 @@
 const goods = [
-    { title: 'Shirt', price: 150, description: "flkkfklfjkfd dd dd ddddddd d ddfggd  wdfd d ggdgd ", path: "static/images/card/1.jpg"},
-    { title: 'Socks', price: 50, description: "flkkfklfjkfdjk", path: "static/images/card/1.jpg" },
-    { title: 'Jacket', price: 350, description: "flkkfklfjkfdjk", path: "static/images/card/1.jpg" },
-    { title: 'Shoes', price: 250, description: "flkkfklfjkfdjk", path: "static/images/card/1.jpg" },
+    { title: 'Shirt', price: 150, path: "static/images/card/1.jpg"},
+    { title: 'Socks', price: 50, path: "static/images/card/1.jpg" },
+    { title: 'Jacket', price: 350, path: "static/images/card/1.jpg" },
+    { title: 'Shoes', price: 250, path: "static/images/card/1.jpg" },
+    { title: 'Shoes', price: 250, path: "static/images/card/1.jpg" },
+    { title: 'Shoes', price: 250, path: "static/images/card/1.jpg" }
 ];
 
-const renderGoodsItem = (title, price, description, path) => {
-    return `
+class GoodsItem{
+    constructor({title:t, price:p, path}) {
+        this.title = t;
+        this.price = p;
+        this.path = path;
+    }
+
+    render(){
+        return `
     <article class="goods-item">
         <div class="cardImg">
-            <img src="${path}" alt="#">
+            <img src="${this.path}" alt="#">
             <div class="cardImgDark">
                 <button>
                     <img src="static/images/header/cart.png" alt="">
@@ -19,13 +28,10 @@ const renderGoodsItem = (title, price, description, path) => {
         </div>
         <div class="cardInfo">
             <div class="cardInfoTitle">
-                ${title}
-            </div>
-            <div class="cardInfoDescription">
-                ${description}
+                ${this.title}
             </div>
             <div class="cardInfoPrice">
-                ${price}
+                ${this.price}
             </div>
             <button class="cardBuy">
                 Купить
@@ -33,13 +39,32 @@ const renderGoodsItem = (title, price, description, path) => {
         </div>
     </article>
   `;
-};
+    }
 
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.description, item.path));
-    goodsList = goodsList.join('');
-    console.log(goodsList);
-    document.querySelector('.goods-list').innerHTML = goodsList;
 }
 
-renderGoodsList(goods);
+class GoodsList{
+    fetchGoods(){
+       this.goods = goods;
+    }
+    render(){
+        let innerHtml = '';
+        innerHtml = this.goods.map(item => (new GoodsItem(item)).render());
+        document.querySelector('.goods-list').innerHTML = innerHtml.join('');
+    }
+
+    calculatePrice(){
+        let totalPrice = 0;
+        if(this.goods.length>0){
+            for(let i =0;i<goods.length;i++){
+                totalPrice += goods[i].price;
+            }
+        }
+        return totalPrice;
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+$('#totalPrice').text(list.calculatePrice());
